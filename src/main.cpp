b9518b800d05e6758fe803174587a8f159dfdc1d
Joseph <heron.joseph@gmail.com>
2013-02-18 12:25:08 -0500

    MODIFIED: uml diagram
    Change all instances of String to string, and boolean to bool

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "main.h"
+#include <string>
 
-main::main(String userAccountIn, String availableTicketIn, String dailyTransaction, String userAccountOut, String availableTicketOut) {
-	throw "Not yet implemented";
+using namespace std;
+
+int main(string userAccountIn, string availableTicketIn, string dailyTransaction, string userAccountOut, string availableTicketOut)
+{
+	return 0;
 }
--- src/main.h
+++
@@ -1,6 +0,0 @@
-class main {
-
-
-public:
-	main(String userAccountIn, String availableTicketIn, String dailyTransaction, String userAccountOut, String availableTicketOut);
-};
