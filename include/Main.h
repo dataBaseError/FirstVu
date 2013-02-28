2cc592eacae6a07a24ffffd3d492f479ecbd8369
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-02-28 14:00:15 -0500

    ADDED: function to tokenize strings. atm, I'm a bit uneasy about Poco. https://en.wikipedia.org/wiki/Paris_syndrome

--- include/Main.h
+++ include/Main.h
@@ -1,11 +1,19 @@
 #include <string>
+#include <sstream>
 #include <iostream>
 
 #include "Transaction.h"
-#include "../lib/include/Poco/StringTokenizer.h"
+//#include "../lib/include/Poco/StringTokenizer.h"
 
 using namespace std;
 
+// puts the results in an already constructed vector
+vector<string> &split(const string &s, char delim, vector<string> &elems);
+
+//returns a new vector
+vector<std::string> split(const std::string &s, char delim);
+
+int main (int argc, char** argv);
 /*class main {
 
 public:*/
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,22 @@
 #include "../include/Main.h"
 
-using Poco::StringTokenizer;
+//using Poco::StringTokenizer;
+
+vector<string> &split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while(getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
+vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    return split(s, delim, elems);
+}
+
 int main (int argc, char** argv) {
 
     //Transaction* session = new Transaction("", "");
@@ -16,7 +32,8 @@ int main (int argc, char** argv) {
     cout << "Enter your command" << endl;
     cin >> input;
 
-    StringTokenizer inputTokenizer (",ab,cd,", ",");
+    //StringTokenizer inputTokenizer (",ab,cd,", ",");
+    vector<string> x = split(",ab,cd,", ',');
 
 	return 0;
 }
