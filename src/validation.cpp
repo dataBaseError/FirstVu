8826e4b8c16b3b99c98a1d9bc89953ca9b4ed02b
Joseph <heron.joseph@gmail.com>
2013-03-04 15:22:28 -0500

    MODIFIED: documenation, and some minor bugs

--- src/validation.cpp
+++ src/validation.cpp
@@ -1,12 +1,31 @@
-/*
- * validation.cpp
+/**
+ * A file that contains methods for validating input.
  *
- *  Created on: 2013-03-04
- *      Author: capncanuck
+ * @author Ryan Crawford
+ * @author Khalil Fazal
+ * @author Joseph Heron
+ * @author Carly Marshall
  */
-
-#include "validation.h"
+#include "../include/validation.h"
 
 bool validBalance(double balance) {
     throw "Needs to be implemented";
+}
+
+bool validUsername(string username) {
+	throw "Needs to be implemented";
+}
+
+bool validEventName(string event) {
+	throw "Needs to be implemented";
+}
+
+
+bool validAccountType(string type) {
+	throw "Needs to be implemented";
+}
+
+
+bool validPrice(double price) {
+	throw "Needs to be implemented";
 }
