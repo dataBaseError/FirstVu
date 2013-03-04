0fa73ca77311760f5ab6afaabf5065aa0e429edd
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-03-04 16:17:41 -0500

    FIXED: use global uao and ato

--- testSuite.sh
+++ testSuite.sh
@@ -2,6 +2,8 @@
 
 export BINARY="$(readlink -f "./Debug/FirstVu")"
 TESTDIR="./tests"
+export GLOBAL_UAO="$TESTDIR""/glob_account.inp"
+export GLOBAL_ATO="$TESTDIR""/glob_available_tickets.inp"
 
 function clean() {
     find "$TESTDIR" -type f | grep ".dtf." | xargs rm 2> /dev/null
@@ -12,14 +14,22 @@ function testCase() {
     cd "$1"
     case="$(basename $PWD)"
 
-    uao="$case"".uao"
-    ato="$case"".ato"
+    #Input
+    #GLOBAL_UAO
+    #GLOBAL_ATO
     inp="$case"".inp"
+
+    #Output
     dtf="$(mktemp "$case"".dtf.XXX")"
     out="$(mktemp "$case"".out.XXX")"
+    etf="$case"".etf"
+
+    #Expected
+    uao="$case"".uao"
+    ato="$case"".ato"
 
     echo "$case"
-    "$BINARY" "$uao" "$ato" "$dtf" < "$inp" > "$out"
+    "$BINARY" "$GLOBAL_UAO" "$GLOBAL_ATO" "$dtf" < "$inp" > "$out"
 
     rm "$dtf"
     rm "$out"
