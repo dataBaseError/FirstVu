596ecc2cf8e91e53e816c206c5943b0186b91e93
Joseph <heron.joseph@gmail.com>
2013-03-07 12:35:11 -0500

    ADDED: some error messages as strings.

--- include/Strings.h
+++ include/Strings.h
@@ -2,6 +2,10 @@
 #ifndef Strings_H_
 #define Strings_H_
 
+#include <string>
 
+using namespace std;
+
+static const string ENTER_USERNAME;
 
 #endif /* Strings_H_ */
---
+++ src/Strings.cpp
@@ -0,0 +1,17 @@
+#include "../Strings.h"
+
+const string ENTER_USERNAME = "enter username";
+const string CREATE_ENTER_USERNAME = "enter username to create";
+const string ENTER_ACCOUNT_TYPE = "enter account type";
+
+
+const string LOGIN_SUCCESS = "login successful";
+const string LOGOUT_SUCCESS = "logout successful";
+
+
+const string INVALID_USERNAME = "error: invalid username";
+const string INVALID_COMMAND = "error: invalid command";
+const string INVALID_ACCOUNT_TYPE = "error: invalid account type";
+
+const string USER_NOT_LOGGED_IN = "error: no users logged in";
+const string USER_ALREADY_LOGGED = "error: user is already logged in";
