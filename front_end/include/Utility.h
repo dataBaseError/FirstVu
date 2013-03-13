afb6248aeacd56171b137b3ee050c3f947f00715
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-03-12 20:28:32 -0400

    REFACTOR: using Poco::StringTokenizer to split strings

--- front_end/include/Utility.h
+++ front_end/include/Utility.h
@@ -25,24 +25,4 @@ using namespace std;
  */
 string &rtrim(string &s);
 
-/**
- * Puts the results in an already constructed vector
- * http://www.cplusplus.com/faq/sequences/strings/split/
- *
- * @param s the remaining string
- * @param delim a delimiter where a string will be splitted
- * @param elems a collection of accumulated tokens
- * @return a new vector of strings
- */
-vector<string> &split(const string &s, char delim, vector<string> &elems);
-
-/*
- * Splits a string by a delimiter
- *
- * @param s a string separated by the delimiter
- * @param delim a delimiter where a string will be splitted
- * @return a new vector
- */
-vector<string> split(const string &s, char delim);
-
 #endif /* UTILITY_H_ */
