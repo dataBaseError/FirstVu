e44aa0b679ae07eadc15b3385fb481923f74f617
carlymarshmallow <carlymarshmallow@gmail.com>
2013-03-22 14:24:36 -0400

    MODIFIED: documented Ticket class

--- back_end/src/Ticket.java
+++ back_end/src/Ticket.java
@@ -1,72 +1,99 @@
+/**
+ * Class for holding information involving tickets for sale in the program. 
+ * @author Ryan Crawford
+ * @author Khalil Fazal
+ * @author Joseph Heron
+ * @author Carly Marshall
+ */
+
 public class Ticket {
 
 	private string event;
-	private string userName;
+	private string username;
 	private int ticketNumber;
 	private double cost;
 
 	/**
-	 * 
-	 * @param event
-	 * @param username
-	 * @param ticketNumber
-	 * @param cost
+	 * Constructor for making a ticket listing for an event.
+	 * @param event The name of the event.
+	 * @param username The username of the seller.
+	 * @param ticketNumber The number of tickets available.
+	 * @param cost The cost per ticket.
 	 */
 	public Ticket(string event, string username, int ticketNumber, double cost) {
-		throw new UnsupportedOperationException();
+		this.event = event;
+		this.username = username;
+		this.ticketNumber = ticketNumber;
+		this.cost = cost;
 	}
 
+	/**
+	 * Accessor method for getting the event name.
+	 * @return The name of the event.
+	 */
 	public string getEvent() {
 		return this.event;
 	}
 
 	/**
-	 * 
-	 * @param event
+	 * Mutator method for the event name.
+	 * @param event The name of the event.
 	 */
 	public void setEvent(string event) {
 		this.event = event;
 	}
 
+	/**
+	 * Accessor method for getting the seller’s username.
+	 * @return The username of the seller.
+	 */
 	public string getUsername() {
-		throw new UnsupportedOperationException();
+		return this.username;
 	}
 
 	/**
-	 * 
-	 * @param username
+	 * Mutator method for the seller’s username.
+	 * @param username The username of the seller.
 	 */
 	public void setUsername(string username) {
-		throw new UnsupportedOperationException();
+		this.username = username;
 	}
 
+	/**
+	 * Accessor method for getting the number of tickets available for the show.
+	 * @return The number of available tickets.
+	 */
 	public int getTicketNumber() {
 		return this.ticketNumber;
 	}
 
 	/**
-	 * 
-	 * @param ticketNumber
+	 * Mutator method for number of remaining tickets for sale.
+	 * @param ticketNumber The number of available tickets.
 	 */
 	public void setTicketNumber(int ticketNumber) {
 		this.ticketNumber = ticketNumber;
 	}
 
 	/**
-	 * 
-	 * @param ticketNumber
+	 * Decrement the number of tickets for sale for the event.
+	 * @param ticketNumber The number of available tickets.
 	 */
 	public void decreaseTicketNumber(int ticketNumber) {
-		throw new UnsupportedOperationException();
+		this.ticketNumber -= ticketNumber;
 	}
 
+	/**
+	 * Accessor method for getting the cost of the event per ticket.
+	 * @return Cost per ticket.
+	 */
 	public double getCost() {
 		return this.cost;
 	}
 
 	/**
-	 * 
-	 * @param cost
+	 * Mutator method for the cost per ticket for the event.
+	 * @param cost Cost per ticket.
 	 */
 	public void setCost(double cost) {
 		this.cost = cost;
