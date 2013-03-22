280622ecc1bf7b1643872e91299db27f1f96d347
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-03-22 14:02:23 -0400

    FIXED: string -> String

--- back_end/src/EventTransaction.java
+++ back_end/src/EventTransaction.java
@@ -1,72 +1,74 @@
 public class EventTransaction extends Entry {
 
-	private string eventName;
-	private string sellName;
-	private double price;
-	private int numTickets;
+    private String eventName;
+    private String sellName;
+    private double price;
+    private int numTickets;
 
-	/**
-	 * 
-	 * @param type
-	 * @param eventName
-	 * @param sellName
-	 * @param price
-	 * @param numTickets
-	 */
-	public EventTransaction(int type, string eventName, string sellName, double price, int numTickets) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * 
+     * @param type
+     * @param eventName
+     * @param sellName
+     * @param price
+     * @param numTickets
+     */
+    public EventTransaction(final int type, final String eventName, final String sellName, final double price, final int numTickets) {
+        super(type);
+        throw new UnsupportedOperationException();
+    }
 
-	public string getSellName() {
-		return this.sellName;
-	}
+    public String getSellName() {
+        return this.sellName;
+    }
 
-	/**
-	 * 
-	 * @param sellName
-	 */
-	public void setSellName(string sellName) {
-		this.sellName = sellName;
-	}
+    /**
+     * 
+     * @param sellName
+     */
+    public void setSellName(final String sellName) {
+        this.sellName = sellName;
+    }
 
-	public string getEventName() {
-		return this.eventName;
-	}
+    public String getEventName() {
+        return this.eventName;
+    }
 
-	/**
-	 * 
-	 * @param eventName
-	 */
-	public void setEventName(string eventName) {
-		this.eventName = eventName;
-	}
+    /**
+     * 
+     * @param eventName
+     */
+    public void setEventName(final String eventName) {
+        this.eventName = eventName;
+    }
 
-	public double getPrice() {
-		return this.price;
-	}
+    public double getPrice() {
+        return this.price;
+    }
 
-	/**
-	 * 
-	 * @param price
-	 */
-	public void setPrice(double price) {
-		this.price = price;
-	}
+    /**
+     * 
+     * @param price
+     */
+    public void setPrice(final double price) {
+        this.price = price;
+    }
 
-	public int getNumTickets() {
-		return this.numTickets;
-	}
+    public int getNumTickets() {
+        return this.numTickets;
+    }
 
-	/**
-	 * 
-	 * @param numTickets
-	 */
-	public void setNumTickets(int numTickets) {
-		this.numTickets = numTickets;
-	}
+    /**
+     * 
+     * @param numTickets
+     */
+    public void setNumTickets(final int numTickets) {
+        this.numTickets = numTickets;
+    }
 
-	public string getDTFLine() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public String getDTFLine() {
+        throw new UnsupportedOperationException();
+    }
 
 }
