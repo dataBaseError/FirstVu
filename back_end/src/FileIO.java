0b2dc572591292899eedc2069f13848f4fe881e6
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-03-28 12:45:21 -0400

    FIXED: syntax errors

--- back_end/src/FileIO.java
+++ back_end/src/FileIO.java
@@ -1,19 +1,58 @@
-import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Handles all file inputs and output
+ * 
+ * @author Ryan Crawford
+ * @author Khalil Fazal
+ * @author Joseph Heron
+ * @author Carly Marshall
+ */
 public class FileIO {
 
-    private ArrayList<Account> accountList;
-    private ArrayList<Ticket> eventList;
-    private String accountLocation;
-    private String ticketLocation;
-    private String transactionLocation;
-    private String newAccountLocation;
-    private String newTicketLocation;
+    /**
+     * A collection of user accounts
+     */
+    private final ArrayList<Account> accountList;
+
+    /**
+     * A collection of events
+     */
+    private final ArrayList<Ticket> eventList;
+
+    /**
+     * The location of the current User Accounts file.
+     */
+    private final String accountLocation;
+
+    /**
+     * The location of the current Available Tickets file.
+     */
+    private final String ticketLocation;
+
+    /**
+     * The location of the daily transaction file.
+     */
+    private final String transactionLocation;
+
+    /**
+     * The location for the new User Accounts file to
+     * be stored to. 
+     */
+    private final String newAccountLocation;
+
+    /**
+     * The location for the new Available Tickets file
+     * to be stored to.
+     */
+    private final String newTicketLocation;
 
     /**
      * Constructor for the file input and output.
+     * @param transactionLocation The location of the daily transaction file.
      * @param accountLocation The location of the current User Accounts file.
      * @param ticketLocation The location of the current Available Tickets file.
      * @param newAccountLocation The location for the new User Accounts file to
@@ -24,9 +63,9 @@ public class FileIO {
     public FileIO(final String transactionLocation, final String accountLocation, final String ticketLocation, final String newAccountLocation, final String newTicketLocation) {
         this.accountList = new ArrayList<Account>();
         this.eventList = new ArrayList<Ticket>();
-        
-    	this.accountLocation = accountLocation;
-    	this.transactionLocation = transactionLocation;
+
+        this.accountLocation = accountLocation;
+        this.transactionLocation = transactionLocation;
         this.ticketLocation = ticketLocation;
         this.newAccountLocation = newAccountLocation;
         this.newTicketLocation = newTicketLocation;
@@ -41,15 +80,6 @@ public class FileIO {
     }
 
     /**
-     * MIGHT NOT BE NEEDED
-     * Mutator method for the current user account list.
-     * @param accountList The new list of accounts.
-     */
-    public void setAccountList(final ArrayList<Account> accountList) {
-        this.accountList = accountList;
-    }
-
-    /**
      * Accessor for the list of currently available tickets.
      * @return The list of currently available tickets.
      */
@@ -58,37 +88,28 @@ public class FileIO {
     }
 
     /**
-     * MIGHT NOT BE NEEDED
-     * Mutator for the list of currently available tickets.
-     * @param eventList The new list of currently available tickets.
-     */
-    public void setEventList(final ArrayList<Ticket> eventList) {
-        this.eventList = eventList;
-    }
-
-    /**
      * Read in the User Accounts file.
      * @return Whether the file was successfully read or not.
      */
     public boolean readAccountFile() {
-    	String line = "";
-    	String[] lineArray = new String[3];
-    	Double balance;
-    	try{
-    	BufferedReader fStream = new BufferedReader (new FileReader(this.accountLocation));
-    	while((line = fStream.readLine()) != null){
-    		lineArray =line.split("\\s+");
-    		balance = (Double.parseDouble(lineArray[2]));
-    		accountList.add(new Account(lineArray[0],lineArray[1],balance));
-    	}
-    	
-    	fStream.close();
-    	}catch(IOException e){
-    		e.printStackTrace();
-    		return false;
-    	}
-    	return true;
-    	
+        String line = "";
+        String[] lineArray = new String[3];
+        Double balance;
+        try {
+            final BufferedReader fStream = new BufferedReader(new FileReader(this.accountLocation));
+            while ((line = fStream.readLine()) != null) {
+                lineArray = line.split("\\s+");
+                balance = Double.parseDouble(lineArray[2]);
+                this.accountList.add(new Account(lineArray[0], lineArray[1], balance));
+            }
+
+            fStream.close();
+        } catch (final IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+
     }
 
     /**
@@ -96,26 +117,26 @@ public class FileIO {
      * @return Whether the file was successfully read or not.
      */
     public boolean readTicketFile() {
-    	String line = "";
-    	String[] lineArray = new String[4];
-    	int ticketNumber;
-    	Double cost;
-    	try{
-    	BufferedReader fStream = new BufferedReader (new FileReader(this.ticketLocation));
-    	while((line = fStream.readLine()) != null){
-    		lineArray =line.split("\\s+");
-    		ticketNumber = (Integer.parseInt(lineArray[2]));
-    		cost = (Double.parseDouble(lineArray[3]));
-    		eventList.add(new Ticket(lineArray[0],lineArray[1],ticketNumber,cost));
-    	}
-    	
-    	fStream.close();
-    	}catch(IOException e){
-    		e.printStackTrace();
-    		return false;
-    	}
-    	return true;
-        
+        String line = "";
+        String[] lineArray = new String[4];
+        int ticketNumber;
+        Double cost;
+        try {
+            final BufferedReader fStream = new BufferedReader(new FileReader(this.ticketLocation));
+            while ((line = fStream.readLine()) != null) {
+                lineArray = line.split("\\s+");
+                ticketNumber = Integer.parseInt(lineArray[2]);
+                cost = Double.parseDouble(lineArray[3]);
+                this.eventList.add(new Ticket(lineArray[0], lineArray[1], ticketNumber, cost));
+            }
+
+            fStream.close();
+        } catch (final IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+
     }
 
     /**
@@ -139,70 +160,56 @@ public class FileIO {
      * @return The list of transactions to apply to the User Accounts and 
      * Available Tickets file. 
      */
-    //UNFINISHED
+    // UNFINISHED
     public ArrayList<Entry> readTransactions() {
-    	String line = "";
-    	String[] lineArray = new String[5];
-    	double balance;
-    	int numTickets;
-    	double price;
-    	try{
-    	BufferedReader fStream = new BufferedReader (new FileReader(this.transactionLocation));
-    	while((line = fStream.readLine()) != null){
-    		lineArray =line.split("\\s+");
-    		int transaction = (Integer.parseInt(lineArray[0]));
-    		String username = lineArray[1];
-    		
-    		switch(transaction) {
-    		case 0:    		
-    			balance = (Double.parseDouble(lineArray[3]));
-    			new AuxiliaryTransaction(transaction,username,balance,lineArray[2]);
-    			break;
-    			//logout
-    		case 1:
-    			balance = (Double.parseDouble(lineArray[3]));
-    			new AuxiliaryTransaction(transaction,username,balance,lineArray[2]);
-    			break;
-    			//create
-    		case 2:
-    			balance = (Double.parseDouble(lineArray[3]));
-    			new AuxiliaryTransaction(transaction,username,balance,lineArray[2]);
-    			break;
-    			//delete
-    		case 3:
-    			numTickets = (Integer.parseInt(lineArray[3]));
-    			price = (Double.parseDouble(lineArray[4]));
-    			new EventTransaction(transaction,lineArray[1],lineArray[2],price,numTickets);
-    			break;
-    			//sell
-    		case 4:
-    			numTickets = (Integer.parseInt(lineArray[3]));
-    			price = (Double.parseDouble(lineArray[4]));
-    			new EventTransaction(transaction,lineArray[1],lineArray[2],price,numTickets);
-    			break;   
-    			//buy
-    		case 5:
-    			double amount = (Double.parseDouble(lineArray[3]));
-    			new Refund(transaction,lineArray[1],lineArray[2],amount);
-    			break;
-    			//refund
-    		case 6:
-    			balance = (Double.parseDouble(lineArray[3]));
-    			new AuxiliaryTransaction(transaction,username,balance,lineArray[2]);
-    			break;
-    			//addcredit
-    		
-    		}
-    		
-    	}
-    	
-    	fStream.close();
-    	}catch(IOException e){
-    		e.printStackTrace();
-    		
-    	}
-		return null;
-    	
+        String line = "";
+        String[] lineArray = new String[5];
+        double balance;
+        int numTickets;
+        double price;
+        try {
+            final BufferedReader fStream = new BufferedReader(new FileReader(this.transactionLocation));
+            while ((line = fStream.readLine()) != null) {
+                lineArray = line.split("\\s+");
+                final int transaction = Integer.parseInt(lineArray[0]);
+                final String username = lineArray[1];
+
+                switch (transaction) {
+                    case 0:
+                        // logout
+                    case 1:
+                        // create
+                    case 2:
+                        // delete
+                    case 6:
+                        // addcredit
+                        balance = Double.parseDouble(lineArray[3]);
+                        new AuxiliaryTransaction(transaction, username, balance, lineArray[2]);
+                        break;
+                    case 3:
+                        // sell
+                    case 4:
+                        // buy
+                        numTickets = Integer.parseInt(lineArray[3]);
+                        price = Double.parseDouble(lineArray[4]);
+                        new EventTransaction(transaction, lineArray[1], lineArray[2], price, numTickets);
+                        break;
+                    case 5:
+                        // refund
+                        final double amount = Double.parseDouble(lineArray[3]);
+                        new Refund(transaction, lineArray[1], lineArray[2], amount);
+                        break;
+                }
+
+            }
+
+            fStream.close();
+        } catch (final IOException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+
     }
 
     /**
