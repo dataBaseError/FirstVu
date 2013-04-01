2576a71011ae2fb9c688fc9e8730ad0565cbe1ec
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-04-01 16:15:27 -0400

    IMPLEMENTED: ErrorMessages#printError

--- back_end/src/ErrorMessages.java
+++ back_end/src/ErrorMessages.java
@@ -10,13 +10,8 @@ public class ErrorMessages {
     /**
      * @param type the type of error message
      * @param message the error message
-     * @param file the file that is involved in the error, null if no file is involved
      */
-    public static void printError(final String type, final String message, final String file) {
-        if (file == null) {
-            // Fatal Error
-        } else {
-            // Constraint Error
-        }
+    public static void printError(final String type, final String message) {
+        System.out.printf("%s error: %s\n", type, message);
     }
 }
