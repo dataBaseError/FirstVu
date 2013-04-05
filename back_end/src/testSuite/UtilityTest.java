d9d208e0bf09038b1192bf7d64caf9f891db6819
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-04-05 14:21:55 -0400

    TESTED: Utility

--- back_end/src/testSuite/UtilityTest.java
+++ back_end/src/testSuite/UtilityTest.java
@@ -1,30 +1,47 @@
 package testSuite;
 
-import static org.junit.Assert.*;
-import junit.framework.Assert;
-
-import main.Account;
-import main.AuxiliaryTransaction;
+import static org.junit.Assert.assertEquals;
 import main.Utility;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Tests the Utility class which contains only static methods
+ * 
+ * @author Ryan Crawford
+ * @author Khalil Fazal
+ * @author Joseph Heron
+ * @author Carly Marshall
+ */
 public class UtilityTest {
 
-	private String test;
-	
-	/**
+    /**
+     * The expected line
+     */
+    private String line;
+
+    /**
      * Set up the AuxiliaryTransaction transaction
      */
     @Before
     public void setUp() {
-        test = "0000000000";
-    }  
-    
-	@Test
-	public void testCreateLine() {
-		assertEquals(Utility.createLine('0', 10), test);
-	}
+        this.line = "0000000000";
+    }
 
+    /**
+     * Test to see if Utility's constructor is visible
+     */
+    @Test()
+    public void coverUtility() {
+        new Utility();
+    }
+
+    /**
+     * Tests the creation of a line of characters
+     */
+    @Test
+    public void createLine() {
+        assertEquals(Utility.createLine('0', 10), this.line);
+    }
 }
