0dd89e85c125d7949c58ecc26dd655e2b8abef96
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-04-11 17:36:05 -0400

    ADDED: CondLoopTest to the TestSuite

--- back_end/src/testSuite/AllTests.java
+++ back_end/src/testSuite/AllTests.java
@@ -17,6 +17,7 @@ import org.junit.runners.Suite.SuiteClasses;
         AccountTest.class,
         AuxiliaryTransactionTest.class,
         BackendTest.class,
+        CondLoopTest.class,
         ErrorMessagesTest.class,
         EventTransactionTest.class,
         FileIOTest.class,
